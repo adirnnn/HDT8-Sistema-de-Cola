9b2a3623d8cee06a469058fb3c284c2e8311ff47
unknown <28adrianls@gmail.com>
2024-04-07 14:53:32 -0600

    JUnit Test and Documentation

--- hdt8/src/test/java/uvg/edu/gt/AppTest.java
+++ hdt8/src/test/java/uvg/edu/gt/AppTest.java
@@ -1,20 +1,26 @@
 package uvg.edu.gt;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAdd() {
+        VectorHeap<Integer> heap = new VectorHeap<>();
+        heap.add(5);
+        heap.add(3);
+        heap.add(8);
+        assertEquals(3, heap.getFirst().intValue());
+    }
+    
+    @Test
+    public void testRemove() {
+        VectorHeap<Integer> heap = new VectorHeap<>();
+        heap.add(5);
+        heap.add(3);
+        heap.add(8);
+        assertEquals(3, heap.remove().intValue());
+        assertEquals(5, heap.getFirst().intValue());
     }
 }
